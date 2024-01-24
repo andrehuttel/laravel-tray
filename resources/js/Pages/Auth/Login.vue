2349bf0b0d56d681e8a444de187af068c11aeedd
André <andreluizhuttel@gmail.com>
2024-01-24 19:26:17 -0300

    Include Register button in form login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -85,9 +85,14 @@ const submit = () => {
                     Forgot your password?
                 </Link>
 
+                <Link :href="route('register')" class="inline-flex items-center px-4 py-2 bg-indigo-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    Register
+                </Link>
+
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
                 </PrimaryButton>
+
             </div>
         </form>
     </GuestLayout>
