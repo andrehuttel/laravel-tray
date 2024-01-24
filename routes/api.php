01fd19a04621883235f257dca011894932a2a13d
André <andreluizhuttel@gmail.com>
2024-01-24 20:16:03 -0300

    register in routes/api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SaleController;
+use App\Http\Controllers\SellerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::apiResource('sellers', SellerController::class);
+Route::apiResource('sales', SaleController::class);
+Route::get('sales/seller/{seller}', [SaleController::class, 'showBySeller']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
