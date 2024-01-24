d48851a3fb1dd1b78ec41393a481334a83c0e554
André <andreluizhuttel@gmail.com>
2024-01-24 20:12:07 -0300

    create requests, resources and implement api calls in controllers

---
+++ app/Http/Requests/SaleRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SaleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'seller_id' => $this->isMethod('post') ? 'required|exists:sellers,id' : 'sometimes|exists:sellers,id',
+            'value' => 'required|numeric',
+            'sale_date' => $this->isMethod('post') ? 'required|date' : 'sometimes|date',
+        ];
+    }
+}
